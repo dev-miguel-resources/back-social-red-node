3a8f57ff66313276136fddfc268f9e354aab9750
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-07-10 12:28:42 -0400

    config editorconfig, eslint and prettier

--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,9 @@
+// shift + alt + f: prettier config
 interface Person {
-   name: string 
-   lastname: string
+	name: string;
+	lastname: string;
 }
 
 const person: Person = { name: 'Facundo', lastname: 'Ramirez' };
 console.log(person);
+
