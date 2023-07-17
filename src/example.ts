a210ecd277a5b36c4bbb3ab267f63423da6017e6
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-07-17 12:09:29 -0400

    defining onion architecture

--- src/example.ts
+++
@@ -1,8 +0,0 @@
-// shift + alt + f: prettier config
-interface Person {
-	name: string;
-	lastname: string;
-}
-
-const person: Person = { name: 'Marcelo', lastname: 'Ramirez' };
-console.log(person);
