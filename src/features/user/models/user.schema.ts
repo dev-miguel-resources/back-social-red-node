b30a6c5702e9c808efbac248c634c43ff991f646
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-08-16 11:29:21 -0400

    defining auth model with interfaces

--- src/features/user/models/user.schema.ts
+++ src/features/user/models/user.schema.ts
@@ -7,26 +7,26 @@ const userSchema: Schema = new Schema({
 	postsCount: { type: Number, default: 0 },
 	followersCount: { type: Number, default: 0 },
 	followingCount: { type: Number, default: 0 },
-	blocked: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-	blockedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+	blocked: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+	blockedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 	notifications: {
 		messages: { type: Boolean, default: true },
 		reactions: { type: Boolean, default: true },
 		comments: { type: Boolean, default: true },
-		follows: { type: Boolean, default: true },
+		follows: { type: Boolean, default: true }
 	},
 	social: {
 		facebook: { type: String, default: true },
 		instagram: { type: String, default: true },
 		twitter: { type: String, default: true },
-		youtube: { type: String, default: true },
+		youtube: { type: String, default: true }
 	},
 	work: { type: String, default: '' },
 	school: { type: String, default: '' },
 	location: { type: String, default: '' },
 	quote: { type: String, default: '' },
 	bgImageVersion: { type: String, default: '' },
-	bgImageId: { type: String, default: '' },
+	bgImageId: { type: String, default: '' }
 });
 
 const UserModel: Model<IUserDocument> = model<IUserDocument>('User', userSchema, 'User');
